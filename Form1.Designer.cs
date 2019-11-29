c31832de3df36aaaeee75eefc11454eb93fa6bb1
Steve Aldworth <s.aldworth@tvdsb.ca>
2019-11-29 08:06:40 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Basic_Input
+{
+    partial class BasicInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.trkAgeSelect = new System.Windows.Forms.TrackBar();
+            this.lblAgeSelected = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkAgeSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(176, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Please enter your name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(194, 6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(13, 55);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(163, 20);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "Please enter your age";
+            // 
+            // trkAgeSelect
+            // 
+            this.trkAgeSelect.Location = new System.Drawing.Point(194, 50);
+            this.trkAgeSelect.Maximum = 50;
+            this.trkAgeSelect.Name = "trkAgeSelect";
+            this.trkAgeSelect.Size = new System.Drawing.Size(279, 45);
+            this.trkAgeSelect.TabIndex = 3;
+            this.trkAgeSelect.Scroll += new System.EventHandler(this.TrkAgeSelect_Scroll);
+            // 
+            // lblAgeSelected
+            // 
+            this.lblAgeSelected.AutoSize = true;
+            this.lblAgeSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAgeSelected.Location = new System.Drawing.Point(493, 55);
+            this.lblAgeSelected.Name = "lblAgeSelected";
+            this.lblAgeSelected.Size = new System.Drawing.Size(18, 20);
+            this.lblAgeSelected.TabIndex = 4;
+            this.lblAgeSelected.Text = "0";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(13, 98);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(406, 20);
+            this.lblHeight.TabIndex = 5;
+            this.lblHeight.Text = "Please Enter your height in metres to two decimal places";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.Location = new System.Drawing.Point(426, 95);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(147, 26);
+            this.txtHeight.TabIndex = 6;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(310, 4);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(263, 30);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(13, 146);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 24);
+            this.lblMessage.TabIndex = 8;
+            // 
+            // BasicInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 223);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblAgeSelected);
+            this.Controls.Add(this.trkAgeSelect);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "BasicInput";
+            this.Text = "Basic Input";
+            ((System.ComponentModel.ISupportInitialize)(this.trkAgeSelect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TrackBar trkAgeSelect;
+        private System.Windows.Forms.Label lblAgeSelected;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Part 3 - Basic Input Solution
+namespace Basic_Input
+{
+    public partial class BasicInput : Form
+    {
+        int age;
+        string name;
+        double height;
+        public BasicInput()
+        {
+            InitializeComponent();
+        }
+        private void TrkAgeSelect_Scroll(object sender, EventArgs e)
+        {
+            age = Convert.ToInt32(trkAgeSelect.Value);  //whenever the trackbar is moved, age is updated
+            lblAgeSelected.Text = age + "";             //puts value of age into Label so user knows what the TrackBar has selected
+        }    
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            //note that since age is updated as the TrackBar is moved
+            name = txtName.Text;                            //gets name from TextBox  
+            Double.TryParse(txtHeight.Text, out height);    //keeps program from crashing if no number is entered.
+            //adds output text to label
+            lblMessage.Text = "Hi " + name + ".  You are " + (82 - age) + " years below life expectancy.\nYou are " + (2.72 - height) + "m shorter than the tallest person.";
+        }
+    }
+}
