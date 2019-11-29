7e90dd6b800aaf280a02d653fe15bfc8a5c1b0ac
Steve Aldworth <s.aldworth@tvdsb.ca>
2019-11-29 08:15:56 -0500

    Updated comments to C# convention standards.

--- Form1.cs
+++ Form1.cs
@@ -7,30 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//Part 3 - Basic Input Solution
+//Part 3 - Basic Input Solution.
 namespace Basic_Input
 {
     public partial class BasicInput : Form
     {
         int age;
+        int averageLife = 82;
         string name;
         double height;
+        double tallestHeight = 2.72;
         public BasicInput()
         {
             InitializeComponent();
         }
         private void TrkAgeSelect_Scroll(object sender, EventArgs e)
         {
-            age = Convert.ToInt32(trkAgeSelect.Value);  //whenever the trackbar is moved, age is updated
-            lblAgeSelected.Text = age + "";             //puts value of age into Label so user knows what the TrackBar has selected
+            //Whenever the trackbar is moved, age is updated.
+            age = Convert.ToInt32(trkAgeSelect.Value);
+            //Puts value of age into Label so user knows what the TrackBar has selected.
+            lblAgeSelected.Text = age + "";             
         }    
         private void BtnSubmit_Click(object sender, EventArgs e)
         {
-            //note that since age is updated as the TrackBar is moved
-            name = txtName.Text;                            //gets name from TextBox  
-            Double.TryParse(txtHeight.Text, out height);    //keeps program from crashing if no number is entered.
-            //adds output text to label
-            lblMessage.Text = "Hi " + name + ".  You are " + (82 - age) + " years below life expectancy.\nYou are " + (2.72 - height) + "m shorter than the tallest person.";
+            //Note that age is updated as the TrackBar is moved and does not need to be done in this submit Button.
+            //Gets name from TextBox.
+            name = txtName.Text;
+            //Keeps program from crashing if no number is entered, sets height to zero.
+            Double.TryParse(txtHeight.Text, out height);    
+            //Adds output text to Label.
+            lblMessage.Text = "Hi " + name + ".  You are " + (averageLife - age) + " years below life expectancy.\nYou are " + (tallestHeight - height) + "m shorter than the tallest person.";
         }
     }
 }
